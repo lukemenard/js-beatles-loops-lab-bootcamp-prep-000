e92c78469bdacdd59c5c5935cd20308d707c01ab
Luke Menard <lukemenard@gmail.com>
2019-03-21 21:46:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var array = []
   var i = 0
   for (i; i < musicians.length; i++){
-    array.push(musicians[i] + 'plays' + instruments[i])
+    array.push(musicians[i] + ' plays ' + instruments[i])
   }
   return array
 }
