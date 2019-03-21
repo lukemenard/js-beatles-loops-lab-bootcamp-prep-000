b6d29aa9136f8ee09760757f5869f79f8b7ca959
Luke Menard <lukemenard@gmail.com>
2019-03-21 22:06:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(number){
   var array = []
   do {
-    array.push("I love the Beatles!");
+    array.push("I love the Beatles!")
     number++
   }
   while (number > 15)
